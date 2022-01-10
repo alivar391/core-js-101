fe57cafb7adf782530497a45e940f6b95858563e
alivar391 <patriot391@mail.ru>
2022-01-10 09:41:51 +0300

    feat: complite 1 arr task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -584,8 +584,12 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const res = indexes.reduce((acc, item) => {
+    const result = acc[item];
+    return result;
+  }, arr);
+  return res;
 }
 
 

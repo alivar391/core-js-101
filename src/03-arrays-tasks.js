7d350ceb73d3bb0582fbf3d55e6bc4a9577b9ed7
alivar391 <patriot391@mail.ru>
2022-01-10 09:12:19 +0300

    feat: complite one more arr task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -564,13 +564,11 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
-  // const newArr = [];
-  // console.log(arr);
-  // arr.map((item) => childrenSelector(item));
-  // childrenSelector(arr);
-  // console.log(arr);
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((acc, item) => {
+    childrenSelector(item);
+    return acc.concat(childrenSelector(item));
+  }, []);
 }
 
 
